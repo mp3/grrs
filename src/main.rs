6d2b8220afec19ecda7a9ee1dc6f0f801a90d36e
mp3 <im.in.a.coma.xxx@gmail.com>
2021-04-09 11:38:06 +0000

    parsing cli arguments with StructOpt

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,5 @@ struct Cli {
 }
 
 fn main() {
-    let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line)
-        }
-    }
-
-    let result = std::fs::read_to_string("tesy.txt");
-    match result {
-        Ok(content) => { println!("File content: {}", content); }
-        Err(error) => { println!("Oh noes: {}", error); }
-    }
+    let args = Cli::from_args;
 }
